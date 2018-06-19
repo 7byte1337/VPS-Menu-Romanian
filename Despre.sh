ba24d9571d0da544426c61390a65c2d7383d73a0
7byte1337 <40406824+7byte1337@users.noreply.github.com>
2018-06-19 23:29:17 +0300

    <3

---
+++ Despre.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear 
+
+echo -e "#===============================================#"
+echo -e "#     Scriptul este valabil pentru OS-urile     #"
+echo -e "#-----------------------------------------------#"
+echo -e "#    Ubuntu 14,16 ,DEBIAN 7,8,9, 32bit & 64bit  #"
+echo -e "#                     VPS                       #"
+echo -e "#=====================PERU======================#"
+echo -e "● Date pentru instalare" 
+echo -e "☑️ OpenSSH, port: 22" 
+echo -e "☑️ Dropbear, port: 443" 
+echo -e "☑️ Squid3 Port: 8080, 3128"
+echo -e "☑️ Proxy Python: 1080,53"
+echo -e "☑️ Badvpn: Port:7300" 
+echo -e "☑️ Webmin: http://IPLaVPS:10000/" 
+echo -e "☑️ etc"
+echo -e "Sa te intorci la loc la meniu foloseste : [menu]" 
+echo -e "" 
